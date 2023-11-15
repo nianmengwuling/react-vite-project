39fa6895b190062e66505a0b63e715af5adae1d9
nianmengwuling <18861857104@163.com>
2023-11-15 18:49:06 +0800

    全局样式设置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //样式初始化
 import "reset-css"
+//全局样式
+import"./assets/styles/global.scss"
+//组件样式
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
