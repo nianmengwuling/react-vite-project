1a262e09fc35622b2ab12a457002367cc83d8583
nianmengwuling <18861857104@163.com>
2023-11-15 22:35:23 +0800

    antd的引入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import Comp1 from "@/components/Comp1"
 import Comp2 from "@/components/Comp2"
+import {Button} from "antd"
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         顶级组件
         <Comp1></Comp1>
         <Comp2></Comp2>
+        <Button type="primary">这是一个按钮</Button>
       </div>
     </>
   )
