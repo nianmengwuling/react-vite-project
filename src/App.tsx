6daf80ca8300856b4079e8a29a568542bc8c17e1
nianmengwuling <18861857104@163.com>
2023-11-30 20:38:02 +0800

    重定向

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 //import { useState } from 'react'
-import Comp1 from "@/components/Comp1"
+/* import Comp1 from "@/components/Comp1"
 import Comp2 from "@/components/Comp2"
 import { Button } from "antd"//antd的按钮引入
-import { UpCircleOutlined } from "@ant-design/icons"
-import { Outlet } from "react-router-dom"
+import { UpCircleOutlined } from "@ant-design/icons" */
+import { Outlet, Link } from "react-router-dom"
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         顶级组件
         <Comp1></Comp1>
         <Comp2></Comp2>
         <Button type="primary">这是一个按钮</Button>
         <UpCircleOutlined style={{ fontSize: "40px", color: "red" }}></UpCircleOutlined>
-      </div>
-      {/* 路由出口占位，类似于vue的router-view */ }
+      </div> */}
+
+      <Link to="home">Home</Link>
+      <hr />
+      <Link to="about">About</Link>
+      {/* 路由出口占位，类似于vue的router-view */}
       <Outlet></Outlet>
     </>
   )
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,28 @@
 import App from '@/App';
 import Home from "../views/Home";
 import About from "../views/About";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-const baseRouter = () => (
+/* const baseRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/about" element={<About />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+} */
+
+const baseRouter = () =>
+(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
+                {/* 重定向到home */}
+                <Route path="/" element={<Navigate to="/home" />}></Route>
                 <Route path="/home" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
             </Route>
